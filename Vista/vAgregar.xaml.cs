0f4aaa14b4513e102a3b7c6e98a65221a88c151e
Byron Guallasamin <bguallasamin1999@outlook.com>
2025-05-15 21:07:02 -0500

    Cambio

---
+++ Vista/vAgregar.xaml.cs
@@ -0,0 +1,31 @@
+using System.Net;
+using Android.Webkit;
+using bguallasaminS6.Modelo;
+
+namespace bguallasaminS6.Vista;
+
+public partial class vAgregar : ContentPage
+{
+	public vAgregar()
+	{
+		InitializeComponent();
+	}
+
+    private void btnAgregar_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+            WebClient client = new WebClient();
+			var parametros = new System.Collections.Specialized.NameValueCollection();
+			parametros.Add("nombre", txtNombre.Text);
+			parametros.Add("apellido", txtApellido.Text);
+			client.UploadValues("http://localhost:7878/api/v1/personas", "POST", parametros);
+			Navigation.PushAsync(new vCrud());
+        }
+		catch (Exception ex)
+		{
+
+			DisplayAlert("Error", "No se pudo agregar la persona: " + ex.Message, "OK");
+        }
+    }
+}
