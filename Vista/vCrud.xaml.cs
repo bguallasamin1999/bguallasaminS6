d118d12116dc19fed45d6b73f4d1f3a1f73bdefa
Byron Guallasamin <bguallasamin1999@outlook.com>
2025-05-15 20:54:57 -0500

    Agregar archivos de proyecto.

---
+++ Vista/vCrud.xaml.cs
@@ -0,0 +1,21 @@
+using System.Collections.ObjectModel;
+using bguallasaminS6.Modelo;
+using Newtonsoft.Json;
+
+namespace bguallasaminS6.Vista;
+
+public partial class vCrud : ContentPage
+{	private const string url = "http://localhost:7878/api/v1/personas";
+	private HttpClient client = new HttpClient();
+	private ObservableCollection<Persona> _personaGrid;
+    public vCrud()
+	{
+		InitializeComponent();
+	}
+	public async void MostrarPersonas()
+    {
+        var contenido = await client.GetStringAsync(url);
+		List<Persona> listaPersona = JsonConvert.DeserializeObject<List<Persona>>(contenido);
+        _personaGrid = new ObservableCollection<Persona>(listaPersona);
+    }
+}
